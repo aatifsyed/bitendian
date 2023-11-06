1a462ce3ef7ebfca8223833609cc4104fb7f6bd8
Aatif Syed <aatifsyedyp@gmail.com>
2023-11-06 07:01:14 +0000

    wip

--- src/lib.rs
+++ src/lib.rs
@@ -6,25 +6,23 @@ use core::{
     ops::{Index, IndexMut},
 };
 
-pub trait ByteOrderedInt {
-    type ByteArray: ByteArray;
-
+pub trait ByteOrderedInt<const N: usize> {
     /// Return the memory representation of this integer as a byte array in
     /// little-endian byte order.
-    fn to_le_bytes(self) -> Self::ByteArray;
+    fn to_le_bytes(self) -> [u8; N];
     /// Return the memory representation of this integer as a byte array in
     /// big-endian (network) byte order.
-    fn to_be_bytes(self) -> Self::ByteArray;
+    fn to_be_bytes(self) -> [u8; N];
     /// Return the memory representation of this integer as a byte array in
     /// native byte order.
     ///
     /// As the target platform's native endianness is used, portable code
     /// should use [`Self::to_be_bytes`] or [`Self::to_le_bytes`], as appropriate,
     /// instead.
-    fn to_ne_bytes(self) -> Self::ByteArray;
+    fn to_ne_bytes(self) -> [u8; N];
 
     /// Delegates to the appropriate method according to a run-time endianness.
-    fn to_bytes_endian(self, endian: Endian) -> Self::ByteArray
+    fn to_bytes_endian(self, endian: Endian) -> [u8; N]
     where
         Self: Sized,
     {
@@ -37,20 +35,20 @@ pub trait ByteOrderedInt {
 
     /// Create a native endian integer value from its representation
     /// as a byte array in little endian.
-    fn from_le_bytes(bytes: Self::ByteArray) -> Self;
+    fn from_le_bytes(bytes: [u8; N]) -> Self;
     /// Create a native endian integer value from its representation
     /// as a byte array in big endian.
-    fn from_be_bytes(bytes: Self::ByteArray) -> Self;
+    fn from_be_bytes(bytes: [u8; N]) -> Self;
     /// Create a native endian integer value from its memory representation
     /// as a byte array in native endianness.
     ///
     /// As the target platform's native endianness is used, portable code
     /// likely wants to use [`Self::from_be_bytes`] or [`Self::from_le_bytes`], as
     /// appropriate instead.
-    fn from_ne_bytes(bytes: Self::ByteArray) -> Self;
+    fn from_ne_bytes(bytes: [u8; N]) -> Self;
 
     /// Delegates to the appropriate method according to a run-time endianness.
-    fn from_bytes_endian(bytes: Self::ByteArray, endian: Endian) -> Self
+    fn from_bytes_endian(bytes: [u8; N], endian: Endian) -> Self
     where
         Self: Sized,
     {
@@ -62,37 +60,39 @@ pub trait ByteOrderedInt {
     }
 }
 
-macro_rules! byte_order {
-    ($($ty:ty),* $(,)?) => {
-        $(
-            impl ByteOrderedInt for $ty {
-                type ByteArray = [u8; { core::mem::size_of::<$ty>() }];
-                fn to_le_bytes(self) -> Self::ByteArray {
-                    <$ty>::to_le_bytes(self)
-                }
-                fn to_be_bytes(self) -> Self::ByteArray {
-                    <$ty>::to_be_bytes(self)
-                }
-                fn to_ne_bytes(self) -> Self::ByteArray {
-                    <$ty>::to_ne_bytes(self)
-                }
+macro_rules! byte_ordered_int {
+    ($($width:literal { $($ty:ty),* $(,)? }),* $(,)?) => {
+        $( // each width
+            $( // each type
+                impl ByteOrderedInt<$width> for $ty {
+                    fn to_le_bytes(self) -> [u8; $width] {
+                        <$ty>::to_le_bytes(self)
+                    }
+                    fn to_be_bytes(self) -> [u8; $width] {
+                        <$ty>::to_be_bytes(self)
+                    }
+                    fn to_ne_bytes(self) -> [u8; $width] {
+                        <$ty>::to_ne_bytes(self)
+                    }
 
-                fn from_le_bytes(bytes: Self::ByteArray) -> Self {
-                    <$ty>::from_le_bytes(bytes)
+                    fn from_le_bytes(bytes: [u8; $width]) -> Self {
+                        <$ty>::from_le_bytes(bytes)
+                    }
+                    fn from_be_bytes(bytes: [u8; $width]) -> Self {
+                        <$ty>::from_be_bytes(bytes)
+                    }
+                    fn from_ne_bytes(bytes: [u8; $width]) -> Self {
+                        <$ty>::from_ne_bytes(bytes)
+                    }
                 }
-                fn from_be_bytes(bytes: Self::ByteArray) -> Self {
-                    <$ty>::from_be_bytes(bytes)
-                }
-                fn from_ne_bytes(bytes: Self::ByteArray) -> Self {
-                    <$ty>::from_ne_bytes(bytes)
-                }
-            }
-        )*
+            )* // each type
+        )* // each width
     };
 }
+byte_ordered_int!(1{u8, i8});
 
-byte_order!(u8, u16, u32, u64, u128, usize);
-byte_order!(i8, i16, i32, i64, i128, isize);
+// byte_ordered_int!(u8, u16, u32, u64, u128, usize);
+// byte_ordered_int!(i8, i16, i32, i64, i128, isize);
 
 #[derive(Debug, Clone, Copy, Hash, PartialEq, Eq, PartialOrd, Ord, Default)]
 pub enum Endian {
@@ -101,27 +101,4 @@ pub enum Endian {
     Big,
     #[default]
     Native,
-}
-
-pub trait ByteArray
-where
-    Self: IntoIterator<Item = u8>,
-    // Self::IntoIter: ExactSizeIterator,
-    Self: Copy + Hash + Default,
-    Self: PartialEq + Eq + PartialEq<[u8]>,
-    Self: PartialOrd + Ord,
-    Self: AsRef<[u8]> + AsMut<[u8]>,
-    Self: Borrow<[u8]> + BorrowMut<[u8]>,
-    Self: Index<usize> + IndexMut<usize>,
-    Self: for<'a> TryFrom<&'a [u8]>,
-    Self: for<'a> TryFrom<&'a mut [u8]>,
-{
-    const WIDTH: usize;
-}
-
-impl<const N: usize> ByteArray for [u8; N]
-where
-    Self: Default,
-{
-    const WIDTH: usize = N;
 }
